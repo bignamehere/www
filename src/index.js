9b12e0b3aeea6472f336c207ff4d13a51862ceac
Casey Hill <bignamehere@gmail.com>
2018-09-26 12:16:10 -0500

    initialize repo

---
+++ src/index.js
@@ -0,0 +1,17 @@
+////////////////////////////////////////////////////////////////////////////////
+'use strict';
+
+//
+import jQuery from 'jquery';
+
+// FSA-Style imports from node_modules - variables set in webpack.common.js
+import 'FSA-STYLE-SCSS';
+import 'FSA-STYLE-JS';
+
+
+// Project-specific styles
+
+
+// Project-specific JS
+//import './js/docs';
+//import './js/prototype';
