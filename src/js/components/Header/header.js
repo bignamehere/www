54db208a8360dd0a08bcaad14587912f8f16eeb9
Casey Hill <bignamehere@gmail.com>
2018-10-23 22:12:39 -0500

    added icon capabilities

--- src/js/components/Header/header.js
+++ src/js/components/Header/header.js
@@ -2,8 +2,10 @@
 import React, { Component } from "react";
 import { Link } from 'react-router-dom';
 import StartEnd from 'mineral-ui/StartEnd';
+import Flex, {FlexItem} from 'mineral-ui/Flex';
 import Box from 'mineral-ui/Box';
 import Text from 'mineral-ui/Text';
+import IconCode from 'mineral-ui-icons/IconCode';
 import './header.scss';
 
 import Nav from  '../Nav/nav';
@@ -25,7 +27,10 @@ class Header extends Component {
         breakpoints={[474,600]}
         direction={['column','row','row']}>
         <Box>
-          <Text element="h1" fontWeight={300}>{this.state.brandingText}</Text>
+          <Flex direction={['row']}>
+            <FlexItem><IconCode size="2.5em" /></FlexItem>
+            <FlexItem><Text element="h2" fontWeight={300}>{this.state.brandingText}</Text></FlexItem>
+          </Flex>
         </Box>
         <Box>
           <Nav />
